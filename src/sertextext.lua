507b7d9bd5dd21ce74caa413a3a3f144eae5dae9
Ale2610 <Ale2610>
2014-11-18 17:13:34 +0100

    Added Colors!
    Version 1.3! Added Colors!
    
    sertextext.rightColor(2, colors.red, "HI")
    sertextext.slowRightColor(4, colors.yellow, "HELLO!")
    
    sertextext.getInfo()

--- src/sertextext.lua
+++ src/sertextext.lua
@@ -1,5 +1,5 @@
 --SertexText API by Ale2610 and MultMine
---1.2
+--1.3
 
 --normal
 
@@ -60,10 +60,58 @@ function rightColor(y, color, text)
   write(text)
 end
 
-function left(y, color, text)
+function leftColor(y, color, text)
   term.setCursorPos(1, y)
   term.setTextColor(color)
   write(text)
 end
 
+function centerColor(y, color, text)
+	local w, h = term.getSize()
+	term.setCursorPos((w - #text) / 2, y)
+	term.setTextColor(color)
+	write(text)
+end
+
+function CenterDisplayColor(color, text)
+        local x, y = term.getSize()
+        term.setCursorPos(( x - string.len(text)) / 2, y / 2)
+		term.setTextColor(color)
+        write( text )
+end
+
 --slow color
+
+function slowRightColor(y, color, text )
+        local w, h = term.getSize()
+        term.setCursorPos(w - #text, y)
+		term.setTextColor(color)
+        textutils.slowWrite(text)
+end
+
+function slowLeftColor(y, color, text)
+	term.setCursorPos(1, y)
+	term.setTextColor(color)
+	textutils.slowWrite( text )
+end
+
+function slowCenterColor(y, color, text )
+        local w, h = term.getSize()
+        term.setCursorPos((w - #text) / 2, y)
+		term.setTextColor(color)
+        textutils.slowWrite(text)
+end
+
+function slowCenterDisplayColor(color, text)
+        local x, y = term.getSize()
+        term.setCursorPos(( x - string.len(text)) / 2, y / 2)
+		term.setTextColor(color)
+        textutils.slowWrite( text )
+end
+
+--other
+
+function getInfo()
+	write("SertexText API by Ale2610 and MultMine")
+	write("Version: 1.3")
+end
