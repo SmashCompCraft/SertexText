5f22cb45d6230ef68d6423634f7bc1617cc51586
Ale2610 <ale.proto00@gmail.com>
2014-11-19 12:08:38 +0100

    Update sertextext.lua

--- src/1.1/sertextext.lua
+++ src/1.1/sertextext.lua
@@ -1,1 +1,25 @@
-l
+--SertexText API by Ale2610 and MultMine
+--1.1
+
+function right(y, text )
+        local w, h = term.getSize()
+        term.setCursorPos(w - #text, y)
+        write(text)
+end
+
+function left(y, text)
+	term.setCursorPos(1, y)
+	write( text )
+end
+
+function center(y, text )
+        local w, h = term.getSize()
+        term.setCursorPos((w - #text) / 2, y)
+        write(text)
+end
+
+function centerDisplay( text )
+        local x, y = term.getSize()
+        term.setCursorPos(( x - string.len(text)) / 2, y / 2)
+        write( text )
+end
